5c8e4306b48e9fd926a106f65f0e178b4fad7f18
Ryo170844 <ryokub0123310@gmail.com>
2023-12-09 23:06:17 +0700

    เปลี่ยนชื่อผู้ใช้เป็นรหัสผ่าน

--- login/login.php
+++ login/login.php
@@ -22,7 +22,7 @@
         <div class="form">
             <form>
                     <div class="mb-3">
-                        <label for="disabledTextInput" class="form-label">ชื่อผู้ใช้</label>
+                        <label for="disabledTextInput" class="form-label">รหัสผ่าน</label>
                         <input type="text" id="disabledTextInput" class="form-control" placeholder="ชื่อผู้ใช้">
                     </div>
                     <div class="mb-3">
