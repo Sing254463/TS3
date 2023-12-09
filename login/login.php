c78e80527661366a81cd512c022d3985dfff3f02
Sing254463 <sksmaster027@gmail.com>
2023-12-09 22:29:06 +0700

    เพิ่ม ส่วนต่างๆ

--- login/login.php
+++ login/login.php
@@ -1,24 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-<?php
+    <?php
     require("navbarlogin.php");
-    echo str_repeat("<br>",5);
+    echo str_repeat("<br>", 5);
     ?>
-    
-<div class="box">
-    <div class="head">
-        <h3>เข้าสู่ระบบ</h3>
+
+    <div class="box">
+        <div class="head">
+            <h3>เข้าสู่ระบบ</h3>
+        </div>
+
+        <div class="form">
+            <form>
+                    <div class="mb-3">
+                        <label for="disabledTextInput" class="form-label">ชื่อผู้ใช้</label>
+                        <input type="text" id="disabledTextInput" class="form-control" placeholder="ชื่อผู้ใช้">
+                    </div>
+                    <div class="mb-3">
+                        <label for="disabledSelect" class="form-label">รหัสผ่าน</label>
+                        <input type="password" id="disabledTextInput" class="form-control" placeholder="รหัสผ่าน">
+                    </div>
+                    <div class="mb-3">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+        <br>
+        </div>
     </div>
-    <div class="form">
-        
-    </div>
-</div>
 </body>
+
 </html>
