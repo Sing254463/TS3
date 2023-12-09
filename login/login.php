54a40ad826c61cd8a5241eebb7f4985a1c2c1f4b
Sing254463 <sksmaster027@gmail.com>
2023-12-09 21:50:05 +0700

    dirst commit

---
+++ login/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php
+    require("navbarlogin.php");
+    echo str_repeat("<br>",5);
+    ?>
+    
+<div class="box">
+    <div class="head">
+        <h3>เข้าสู่ระบบ</h3>
+    </div>
+    <div class="form">
+    </div>
+</div>
+</body>
+</html>
---
+++ login/navbarlogin.php
@@ -0,0 +1,28 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+<nav class="navbar navbar-expand-lg bg-primary justify-content-center">
+<div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">หน้าหลัก</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">สมาชิก</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">สมัคสมาชิก</a>
+        </li>
+
+      </ul>
+
+      <ul class="navbar-nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link" href="#">Active</a>
+  </li>
+</ul>
+    </div>
+  </div>
+</nav>
