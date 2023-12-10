97d67e898e42b1830ceaec525cedd98b361cda8e
Ryo170844 <ryokub0123310@gmail.com>
2023-12-10 13:04:46 +0700

    หน้าแรกยังไม่เสร็จ

---
+++ home/home.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php
+    ?>
+    <div class="Boxprice">
+        <div class="row">
+            <div class="col-md-3.col-sm-6.col-xs-12.float-shadow">
+                <div class="price_table_box">
+                    <div class="pice_table_head">
+                        "80 USER"
+                        <br>
+                        "จากราคาปกติ"
+                        <br>
+                        <s>
+                            <b>
+                                <font color="red">800</font>
+                            </b>
+                        </s>
+                        "บาท"
+                    </div>
+                    <div class="price_table_body">
+                        <div class="price_table_row.cost.warning-bg">
+                            <strong>40บาท</strong>
+                            <sup>30 วัน</sup>
+                        </div>
+                        <div class="price_table_row">
+                            <strong>ลิขสิทธิ์แท้ (ATHP)</strong>
+                        </div>
+                        <div class="price_table_row"><strong><u>ฟรี</u>ระบบจัดการเซิร์ฟเวอร์</strong>
+                        </div>
+                        <strong>
+                            <div class="price_table_row">
+                                <strong>เพิ่ม,ลดจำนวน User</strong>
+                            </div>
+                            <div class="price_table_row">
+                                <strong>Backup ข้อมูล Server</strong>
+                            </div>
+                            <div class="price_table_row">
+                                <strong>เปลี่ยน IP เองได้ตลอด</strong>
+                            </div>
+                            <div class="price_table_row">
+                                <strong><u>ฟรี</u> Reset Server,Permission</strong>
+                            </div>
+                            <div class="price_table_row">
+                                <strong><u>ฟรี</u> เปลี่ยนแพ็กเกจเองได้ตลอด</strong>
+                            </div>
+                            <div class="price_table_row">
+                                <strong>ไม่จำกัดแบนด์วิชท์เข้า,ออก</strong>
+                            </div>
+                            <div class="price_table_row">
+                                <strong>รองรับโดเมนภาษาไทย</strong>
+                            </div>
+                            <div class="price_table_row">
+                                <strong>รองรับผู้ใช้งานต่างประเทศ 99%
+                                    ระบบสมาชิกสำหรับผู้ใช้งาน
+                                    ต่างประเทศ!!</strong>
+                            </div>
+                            <div class="price_table_row">
+                                <strong><u>ฟรี</u> Firewall (กันยิง)</strong>
+                            </div>
+                            <div class="price_table_row">
+                                <strong>24/7 Support</strong>
+                            </div>
+                        </strong>
+                        <strong>
+                            <a href="" class="btn.btn-warning.btn-lg.btn-block">
+                                <i class="fa fa-shopping-cart"></i>
+                                "เปิดเซิร์ฟเวอร์ เวอร์ชั่นเก่า"
+                                <i class="fa fa-check-square"></i>
+                            </a>
+                            <a href="" class="btn.btn-warning.btn-lg.btn-block">
+                                <i class="fa fa-gift"></i>
+                                "เปิดเซิร์ฟเวอร์ เวอร์ชั่นใหม่"
+                                <i class="fa fa-check-square"></i>
+                                <br>
+                                "เข้าได้ทั้งเวอร์ชั่นเก่าและใหม่"
+                            </a>
+                            <a href="" class="btn.btn-warning.btn-lg.btn-block">
+                                <i class="fa fa-bullhorn"></i>
+                                "เปิดเซิร์ฟเวอร์ เวอร์ชั่นใหม่"
+                                <i class="fa fa-check-square"></i>
+                                <br>
+                                "เข้าใช้งานผ่านโทรศัพท์มือถือได้"
+                            </a>
+                            <a href="" class="btn.btn-warning.btn-lg.btn-block">
+                                <i class="fa fa-users"></i>
+                                "เปิดเซิร์ฟเวอร์ ต่างประเทศ"
+                                <i class="fa fa-check-square"></i>
+                                <br>
+                                "รองรับทั้งไทยและต่างประเทศ"
+                                <br>
+                                "รองรับบอทเพลงทุกค่าย"
+                            </a>
+                            <a href="" class="btn.btn-warning.btn-lg.btn-block">
+                                <i class="fa fa-shopping-cart"></i>
+                                "เช่าบอท TS3 บอทเพลง TS3"
+                                <i class="fa fa-check-square"></i>
+                                <br>
+                                "รองรับทั้ง TS3 เวอร์ชั่นใหม่และเก่า"
+                            </a>
+                        </strong>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+</body>
+
+</html>
