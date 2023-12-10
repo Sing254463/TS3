957e836bb619648b2b676a4ca5ba7af0ee21016a
Sing254463 <sksmaster027@gmail.com>
2023-12-10 22:48:03 +0700

    แก่ไขนิดหน่อย

--- headandfooter/head.php
+++ headandfooter/head.php
@@ -40,8 +40,7 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <div class="collapse navbar-collapse container" id="navbarTogglerDemo03">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
                 <a class="nav-link active" style="text-align: center;" aria-current="page" href="#">
@@ -79,6 +78,10 @@
             </div>
         </div>
     </nav>
+    
+    
+    
+    
     <script>
         window.onscroll = function() {
             myFunction()
