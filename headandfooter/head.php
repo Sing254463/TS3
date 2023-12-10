7f158de2a204d163d91c9d1053985b7b5d190773
Sing254463 <sksmaster027@gmail.com>
2023-12-10 22:43:32 +0700

    ทำ navbar แสร็จแล้วเหลือ ลิ้งไปหน้าต่างๆ

---
+++ headandfooter/head.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="th">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+</head>
+
+
+<body>
+    <div class="container">
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-person-fill"></i>เข้าสู่ระบบ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-person-fill-add"></i>สมัคสมาชิก</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-facebook"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-youtube"></i></a>
+            </li>
+        </ul>
+    </div>
+
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="navbar container">
+            <div class="container" id="navbar">
+                <a class="navbar-brand" href="#">
+                    <img id="linkimg" src="headandfooter\img\logots3thai.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" style="text-align: center;" aria-current="page" href="#">
+                <i class="bi bi-house-fill "></i><br>
+                หน้าแรก</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link"style="text-align: center;" aria-current="page" href="#">
+                <i class="bi bi-box-arrow-right"></i><br>
+                ระบบสมาชิก</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link"style="text-align: center;" aria-current="page" href="#">
+                <i class="bi bi-youtube"></i><br>
+                คลิปสอนเช่า TS3</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="text-align: center;"aria-current="page" href="#">
+                <i class="bi bi-download"></i></i><br>
+                ดาวโหลด TS3</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link"style="text-align: center;" aria-current="page" href="#">
+                <i class="bi bi-cloud-arrow-down-fill"></i></i><br>
+            ดาวโหลด ICON TS3</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link"style="text-align: center;" aria-current="page" href="#">
+                <i class="bi bi-telephone-fill"></i><br>
+            ติดต่อสอบถาม</a>
+            </li>
+        </ul>
+    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <script>
+        window.onscroll = function() {
+            myFunction()
+        };
+
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+
+        function myFunction() {
+            if (window.pageYOffset >= sticky) {
+                navbar.classList.add("sticky")
+            } else {
+                navbar.classList.remove("sticky");
+            }
+        }
+    </script>
--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-</body>
-</html>
+<?php
+require ("headandfooter/head.php");
+?>
+
+<div class="container">
+    <?php
+echo str_repeat("กหฟ<br>", 50);
+    ?>
+</div>
