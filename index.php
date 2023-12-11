c2059ec744d4d88531f569ab76814a2a5aa722f2
Sing254463 <sksmaster027@gmail.com>
2023-12-11 19:09:57 +0700

    row

--- index.php
+++ index.php
@@ -4,7 +4,17 @@ require("headandfooter/head.php");
 
 
 
-
+<div class="container mt-3">
+  <h1>Responsive Columns</h1>
+  <p>Resize the browser window to see the effect.</p>
+  <p>The columns will automatically stack on top of each other when the screen is less than 576px wide.</p>
+  <div class="row">
+    <div class="col-sm-3 p-5 bg-primary text-white">.col</div>
+    <div class="col-sm-3 p-5 bg-dark text-white">.col</div>
+    <div class="col-sm-3 p-5 bg-primary text-white">.col</div>
+    <div class="col-sm-3 p-5 bg-dark text-white">.col</div>
+  </div>
+</div>
 
 
 
